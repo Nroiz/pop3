3e59d4ad6f96520e4c7918bd7b7f629f19b611d8
Noam Roiz <jroiz@Noam-Roizs-MacBook-Pro.local>
2015-01-08 17:46:53 +0200

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Pop3
+//
+//  Created by Noam Roiz on 1/8/15.
+//  Copyright (c) 2015 Noam Roiz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
